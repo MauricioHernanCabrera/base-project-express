5f4390c2a52a85738fbbabff3c0696f4aecd4657
Esdras Pavon <pavonesdras@gmail.com>
2019-11-03 20:10:05 -0300

    activate set interval

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.use((req, res, next) => {
   // database connect
   await initDB();
   // server
-  // dequeueNotes();
+  dequeueNotes();
   const port = process.env.PORT || 3000;
 
   const server = app.listen(port, function() {
